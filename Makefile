7d7a8949729ffabdc8ed1c0cdd0ec939986c0552
Dani <danipozo@correo.ugr.es>
2016-12-09 02:35:23 +0100

    Termina últimas cosas: documentación restante y Makefile.

--- Makefile
+++ Makefile
@@ -6,16 +6,21 @@ CXX = g++
 CPPFLAGS = -Wall -g  -I$(INC) -c
 
 
-all: $(BIN)/pruebacronologia
+all: $(BIN)/union_cronologias $(BIN)/filtradoPalabraClave $(BIN)/filtradoIntervalo
 # ************ Generación de documentación ******************
 documentacion:
 	doxygen doc/doxys/Doxyfile
 
 	
 # ************ Compilación de módulos ************
-$(BIN)/pruebacronologia:
+$(BIN)/union_cronologias:
+	g++ src/union_cronologias.cpp src/cronologia.cpp src/eventohistorico.cpp -o bin/union_cronologias -Iinclude -std=c++14 -g
 
+$(BIN)/filtradoPalabraClave:
+	g++ src/filtradoPalabraclave.cpp src/cronologia.cpp src/eventohistorico.cpp -o bin/filtradoPalabraClave -Iinclude -std=c++14 -g
 
+$(BIN)/filtradoIntervalo:
+	g++ src/filtradoIntervalo.cpp src/cronologia.cpp src/eventohistorico.cpp -o bin/filtradoIntervalo -Iinclude -std=c++14 -g
 
 # ************ Limpieza ************
 clean :
--- include/cronologia.h
+++ include/cronologia.h
@@ -1,28 +1,75 @@
+/**
+ * @file   cronologia.h
+ * @author Daniel Pozo Escalona
+ * 
+ * @brief  Archivo de cabecera del TDA Cronologia.
+ * 
+ * 
+ */
+
 #ifndef CRONOLOGIA_H
 #define CRONOLOGIA_H
 
 #include "eventohistorico.h"
 #include <map>
 
+/**
+ * @brief TDA Cronologia.
+ * 
+ */
+
 class Cronologia
 {
   private:
-    std::map<int, EventoHistorico> eventos;
+    std::map<int, EventoHistorico> eventos; /**< Contenedor asociativo para los eventos. */
 
   public:
     // --- Iteradores.
-    typedef std::map<int, EventoHistorico>::iterator Iterador;
-    typedef std::map<int, EventoHistorico>::const_iterator IteradorConst;
+    typedef std::map<int, EventoHistorico>::iterator Iterador; /**< Tipo iterador. */
+    typedef std::map<int, EventoHistorico>::const_iterator IteradorConst; /**< Tipo iterador constante. */
 
-    Cronologia() = default;
-    Cronologia(const Cronologia& c1, const Cronologia& c2);
+    Cronologia() = default;	/**< Constructor por defecto. */
+    Cronologia(const Cronologia& c1, const Cronologia& c2); /**< Constructor de mezcla. */
 
     // --- Métodos que modifican el estado.
+  /** 
+   * Agrega *e* a la cronología.
+   * 
+   * @param e 
+   */
     void agregarEvento(const EventoHistorico& e) { eventos.insert(std::make_pair(e.obtenerFecha(), e)); }
 
     // --- Métodos que no modifican el estado.
+  /** 
+   * Filtra los eventos que contengan la palabra clave.
+   * 
+   * @param palabraClave 
+   * 
+   * @return Cronología con dichos eventos.
+   */
     Cronologia filtrarEventos(const std::string& palabraClave);
+  /** 
+   * Filtra los eventos cuya fecha esté en [fechaInicio, fechaFinal].
+   * 
+   * @param fechaInicio 
+   * @param fechaFinal 
+   * 
+   * @return Cronología con dichos eventos.
+   */
     Cronologia filtrarEventos(int fechaInicio, int fechaFinal);
+  /** 
+   * Devuelve en los parámetros los siguientes datos:
+   *
+   * - Número de eventos.
+   * - Total de acontecimientos.
+   * - Máximo de acontecimientos en un evento.
+   * - Promedio de acontecimientos en un evento.
+   * 
+   * @param numEventos 
+   * @param totalAcont 
+   * @param maximoAcont 
+   * @param promedioAcont 
+   */
     void estadisticas(int& numEventos, int& totalAcont, int& maximoAcont, double& promedioAcont);
 
     // --- Métodos que devuelven iteradores.
